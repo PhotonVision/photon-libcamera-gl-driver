bd7d2e40794b4dcc7a8d8f54356c7b6b707ada70
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2023-12-28 19:55:20 -0500

    Update camera_model.h

--- include/camera_model.h
+++ include/camera_model.h
@@ -20,9 +20,7 @@
 #include <string>
 #include <vector>
 
-
-public:
-    enum CameraModel {
+enum CameraModel {
         Disconnected = 0,
         OV5647, // Picam v1
         IMX219, // Picam v2
@@ -32,8 +30,6 @@ public:
         OV7251,
         Unknown
     };
-
-    CameraModel stringToModel(const std::string &model);
-    bool isGrayScale(CameraModel model);
-private:
-    CameraModel[] grayScaleCameras = {CameraModel.OV9281};
+CameraModel stringToModel(const std::string &model);
+bool isGrayScale(CameraModel model);
+CameraModel[] grayScaleCameras = {CameraModel.OV9281};
