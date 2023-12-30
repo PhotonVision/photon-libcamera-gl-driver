d3b9e08d84450807ff86bd72c017e9eec8a052d1
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2023-12-30 02:00:54 -0500

    Update blocking_future.h

--- include/blocking_future.h
+++ include/blocking_future.h
@@ -38,7 +38,7 @@ template <typename T> class BlockingFuture {
 
     T take() {
         std::unique_lock<std::mutex> lock(m_mutex);
-        m_cond.wait_for(lock, [&] { return m_data.has_value(); });
+        m_cond.wait(lock, [&] { return m_data.has_value(); });
 
         auto item = std::move(m_data.value());
         m_data.reset();
